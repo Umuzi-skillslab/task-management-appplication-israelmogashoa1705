b99ca74fe9e56f140021ec927bbfb7d2145b87d2
Mogau Mogashoa <israelmogashoa@gmail.com>
2026-06-12 19:33:26 +0200

    Added an id property and a compeltion toggle method

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ let taskCounter = 0;  // Should use let or const
 
 // Task class with errors
 class Task {
-    constructor(title, description, priority) {
+    constructor(id, title, description, priority) {
+        this.id = id;
         this.title = title;
         this.description = description;
         this.priority = priority;
@@ -15,6 +16,9 @@ class Task {
     }
     
     // Missing: method to toggle completion
+    toggleCompletion(){
+        this.completed = !this.completed;
+    }
     
     getInfo() {
         // Wrong string concatenation - should use template literals
