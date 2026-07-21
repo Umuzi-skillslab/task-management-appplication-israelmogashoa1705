312806c520f88322d902a0fc3b3423d47978fd76
Mogau Mogashoa <israelmogashoa@gmail.com>
2026-07-21 09:56:01 +0200

    imported utils modules

--- dom.js
+++ dom.js
@@ -1,5 +1,7 @@
 // Imports the function used to add tasks and the shared task list
 import { addTask, taskList } from "./app.js";
+import { saveToStorage, loadFromStorage } from "./utils.js";
+
 
 // Adds all required event listeners after the page has loaded.
 function setupEventListeners() {
